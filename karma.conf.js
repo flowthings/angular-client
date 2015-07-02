aa3092431ea7f574a6af9e99a9af1e6a1f1273aa
Nathan Faubion <nathan@n-son.com>
2015-07-02 10:40:22 -0500

    Fix rename

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config) {
     files: [
       'node_modules/angular/angular.js',
       'node_modules/angular-mocks/angular-mocks.js',
-      'flowthings.js',
+      'flowthings-angular.js',
       'test/*.js'
     ],
 
